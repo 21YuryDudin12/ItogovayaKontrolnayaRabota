4a229f42472e0b3ed9a9b3385f9667dce65acaa1
Yury Dudin <cepob33@gmail.com>
2023-07-12 10:47:52 +0300

    added 2 methods for filling array and printing array

---
+++ KR/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите количество строчных элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+string[] array = new string[n];
+
+void FillArray(int n, string[] mass)
+{
+for (int i = 0; i<n; i++)
+{
+    Console.WriteLine("Введите элемент под номером "+(i+1)+":");
+    mass[i] = Console.ReadLine()!;
+}
+}
+
+void PrintArray (string[] array)
+{
+    Console.WriteLine("Элементы массива:");
+        foreach (string element in array)
+        {
+            Console.WriteLine(element);
+        }
+}
+
+FillArray(n,array);
+PrintArray(array);
